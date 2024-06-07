51e4957b67602ac01d19dbe1fc258eac63e0081c
JoseSuperior <jose@superiordigital.design>
2024-06-07 13:14:05 -0400

    conteiner

--- db/connection.go
+++ db/connection.go
@@ -1,24 +1,34 @@
 package db
 
 import (
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"os"
 )
 
-var DSN = "host=localhost user=joseSuperior password=a12345678 dbname=tbrdb port=5432"
-
-// DB Se declara la variable afuerade la función para que sea accesible desde cualquier parte del paquete
 var DB *gorm.DB
 
 func ConnectDB() {
 	var err error
-	DB, err = gorm.Open(postgres.Open(DSN), &gorm.Config{})
+
+	// Obtén la configuración de la base de datos de las variables de entorno
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// Construye el DSN
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		dbHost, dbUser, dbPassword, dbName, dbPort)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err)
 	} else {
 		log.Println("Database connected")
 	}
-
 }
